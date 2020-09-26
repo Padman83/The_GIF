e415b140df1a47e705903eb75c2a1a0ba827b680
Padman Selvamanickam <endlessgalaxy15@gmail.com>
2020-09-26 15:33:37 +0800

    Added all files

---
+++ app/src/main/java/com/example/the_gif/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.the_gif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.web_view);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        String file = "file:android_asset/Knight Rider.gif";
+        webView.loadUrl(file);
+    }
+}
